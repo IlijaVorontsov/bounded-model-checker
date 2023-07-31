8cfd73e9d83dff6dfe2cced5a1f89e47d2ffaf77
IlijaVorontsov <ilija.vorontsov@tuta.io>
2023-07-31 15:02:11 +0200

    non-functional implementation of part2

---
+++ Interpolant.py
@@ -0,0 +1,57 @@
+from typing import List
+
+def calculateInterpolant(self, b) -> List[int]:
+    clause_b = b
+    atoms_b = [abs(literal) for literal in b]
+    labels = list()
+    proof_file = open("proof.txt", "r")
+    while True:
+        step_string = proof_file.readline()
+        parts = step_string.split()
+        match parts[1]:
+            case "ROOT":
+                clause = [int(lit) for lit in parts[2:]]
+                if clause == clause_b: # clause is in B
+                    labels.append([[-1]])
+                else:
+                    labels.append([dropLiteralsNotInB(clause, atoms_b)])
+            case "CHAIN":
+                parents, resolved_literals = getChainParentsAndResolvants(step_string)
+                labels.append(labels[parents[0]])
+                for i in range(len(parents) - 1):
+                    if self.resolvedVarNotInB(resolved_literals[i]): # OR over clauses
+                        label = list()
+                        # transform to CNF
+                        for clause_p1 in label[-1]:
+                            for clause_p2 in label[parents[i+1]]:
+                                label.append(clause_p1 + clause_p2) # cross product of clauses (simplification done more efficient by sat solver)
+                        labels[-1] = label
+                    else:  # AND over clauses
+                        labels[-1] = labels[-1] + labels[parents[i+1]] # concat lists 
+        if not step_string:
+            proof_file.close()
+            return labels[-1]
+    
+
+def dropLiteralsNotInB(self, clause, atoms_b):
+    return [literal for literal in clause if abs(literal) in atoms_b]
+
+def resolvedLiteralNotInB(self, resolved_literal, atoms_b):
+    return abs(resolved_literal) not in atoms_b
+
+def getChainParentsAndResolvants(step_string):
+    parts = step_string.split()
+    assert parts[1] == "CHAIN"
+
+    resolved_literals = list()
+    parents = list()
+
+    for i in range(2, len(parts)):
+        if parts[i] == "=>":
+            break
+        elif parts[i][0] == "[":
+            resolved_literals.append(int(parts[i][1:-1])) # remove brackets
+        else:
+            parents.append(int(parts[i]))
+
+    return parents, resolved_literals
--- run-part2.py
+++ run-part2.py
@@ -1,5 +1,5 @@
 from run_part1 import *
-
+from Interpolant import calculateInterpolant
 
 class BMC_with_Interpolants(BoundedModelChecker):
     def __init__(self, circuit: AigerCircuit):
@@ -16,27 +16,46 @@ class BMC_with_Interpolants(BoundedModelChecker):
         self.cnf = CNF()
         self.cnf.add_clauses([1]) # Aiger x_0 is always true (DIMACS variable 1)
         if depth == 0:
-            return self.add_initial_state()
-        
-        self.add_interpolant
+            self.add_initial_state()
+        else:
+            self.last_interpolant = self.interpolant
+            self.interpolant = calculateInterpolant(self.b)
+            for clause in self.interpolant:
+                self.cnf.append(clause)
         self.add_transition_system(depth)
-        self.add_output(depth)
         
-        
+        self.b = [self.literal_at(self.circuit.output, i) for i in range(depth+1)]
+        self.cnf.append(self.b)
+
+        self.cnf.to_file("input.txt")
+        return self.run_minisat()
     
     def check_model(self):
         depth = 0
-        while self.check_depth(depth):
+        while not self.interpolantsConverge():
             if self.check_depth(depth):
-                return depth
+                return False
             else:
                 depth += 1
+        return True
+
+    def interpolantsConverge(self):
+        return self.to_base_literals(self.last_interpolant) == self.to_base_literals(self.interpolant)
+    
+    def to_base_clause(self, clause):
+        base_clause = list()
+        for literal in clause:
+            if literal in [-1, 1]:
+                base_clause.append(literal)
+            elif literal < -1:
+                base_clause.append(-(abs(literal)%self.circuit.maxvar))
+            else:
+                base_clause.append(abs(literal)%self.circuit.maxvar)
+        return base_clause.sort()
 
     def __del__(self):
         run("rm input.txt proof.txt", shell=True)
 
-    
-    
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Run a bounded model checker on the given AIGER file')
