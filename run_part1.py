47f18ace92fd4c946decebc2d7c556805c382f81
IlijaVorontsov <ilija.vorontsov@tuta.io>
2023-08-01 15:54:47 +0200

    refactor

--- run_part1.py
+++ run_part1.py
@@ -8,10 +8,8 @@ class BoundedModelChecker:
     def __init__(self, circuit: AigerCircuit):
         self.circuit = circuit
 
-        self.cnf = CNF()
-        self.cnf.append([1]) # Aiger x_0 is always true (DIMACS literal 1)
-        
-        for latch in self.circuit.latches:
+    def add_initial_state(self):
+        for latch in self.circuit.latches: # i^0 & i^1 & ... & i^latches-1
             self.cnf.append([-latch[0]]) # latch output initialized to 0
 
     def add_transition_system(self, depth: int):
@@ -27,38 +25,35 @@ class BoundedModelChecker:
             self.cnf.append([-self.literal_at(and_gate[0], depth), self.literal_at(and_gate[2], depth)])
             self.cnf.append([self.literal_at(and_gate[0], depth), -self.literal_at(and_gate[1], depth), -self.literal_at(and_gate[2], depth)])   
 
+    def add_output(self, depth: int):
+        # o^0 | o^1 | ... | o^depth
+        self.cnf.append([self.literal_at(self.circuit.output, i) for i in range(depth+1)])
+
     def run_solver(self) -> bool:
+        self.cnf.to_file("input.txt")
         returncode = run("MiniSat-p_v1.14/minisat input.txt > /dev/null", shell=True).returncode
         assert returncode in [10, 20]
+        run("rm input.txt", shell=True)
         return returncode == 10 # 10 if sat, 20 if unsat
 
-    def check_model_at(self, depth: int) -> bool:
-        self.add_transition_system(depth)
+    def check(self, depth: int) -> bool:
+        self.cnf = CNF()
+        self.cnf.append([1]) # Aiger x_0 is always true (DIMACS literal 1)
 
-        tmp_cnf = self.cnf.copy() # copy to incrementally add clauses only output clause changes rest can be appended
-        # o^0 | o^1 | ... | o^depth
-        tmp_cnf.append([self.literal_at(self.circuit.output, i) for i in range(depth+1)])     
-        
-        tmp_cnf.to_file("input.txt")
+        self.add_initial_state()        
+        for i in range(depth+1):
+            self.add_transition_system(i)
+        self.add_output(depth)
+
         return not self.run_solver()
-
-    # returns True if the model is correct, False otherwise
-    def check_model(self, depth) -> bool:        
-        for i in range(depth + 1):
-            if not self.check_model_at(i):
-                return False
-        return True
 
     def literal_at(self, literal: int, depth: int) -> int:
         if literal in [-1, 1]: # constant case
             return literal
         if literal < -1:
-            return literal - depth*self.circuit.maxvar
+            return literal - depth*(self.circuit.maxvar - 1)
         elif literal > 1:
-            return literal + depth*self.circuit.maxvar
-    
-    def __del__(self):
-        run("rm input.txt", shell=True)
+            return literal + depth*(self.circuit.maxvar + 1)
         
 
 if __name__ == '__main__':
@@ -69,7 +64,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     circuit = AigerCircuit(args.filename)
-    if BoundedModelChecker(circuit).check_model(args.k):
+    if BoundedModelChecker(circuit).check(args.k):
         print("OK")
     else:
         print("FAIL")
