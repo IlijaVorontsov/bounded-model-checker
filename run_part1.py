c449a02fa0c1c02d2ff57eee748b52cef48cf9cd
IlijaVorontsov <ilija.vorontsov@tuta.io>
2023-08-08 15:48:24 +0200

    Refactor

--- run_part1.py
+++ run_part1.py
@@ -5,55 +5,76 @@ from AigerCircuit import AigerCircuit
 
 
 class BoundedModelChecker:
-    def __init__(self, circuit: AigerCircuit):
-        self.circuit = circuit
+    def __init__(self, aigerfile: str):
+        self.circuit = AigerCircuit(aigerfile)
+        self.initial_state = [[-latch[0]] for latch in self.circuit.latches]
+        self.property = [[self.circuit.output]]
 
     def add_initial_state(self):
-        for latch in self.circuit.latches: # i^0 & i^1 & ... & i^latches-1
-            self.cnf.append([-latch[0]]) # latch output initialized to 0
+        for clause in self.initial_state:
+            self.cnf.append(clause)
 
-    def add_transition_system(self, depth: int):
-        if depth > 0:
-            for latch in self.circuit.latches:
-                # (o^depth+1 <-> i^depth) == (-o^depth+1 | i^depth) & (o^depth+1 | -i^depth)
-                self.cnf.append([-self.literal_at(latch[0], depth), self.literal_at(latch[1], depth-1)]) 
-                self.cnf.append([self.literal_at(latch[0], depth), -self.literal_at(latch[1], depth-1)])
+    def clauses_transision_system_at(self, depth: int) -> list[list[int]]:
+        clauses = []
+        for latch in self.circuit.latches:
+            # (o^depth+1 <-> i^depth) == (-o^depth+1 | i^depth) & (o^depth+1 | -i^depth)
+            clauses.append([-self.literal_at(latch[0], depth+1), self.literal_at(latch[1], depth)]) 
+            clauses.append([self.literal_at(latch[0], depth+1), -self.literal_at(latch[1], depth)])
 
         for and_gate in self.circuit.and_gates:
             # (o <-> (a1 & a2)) == (-o | a1) & (-o | a2) & (o | -a1 | -a2)
-            self.cnf.append([-self.literal_at(and_gate[0], depth), self.literal_at(and_gate[1], depth)])  
-            self.cnf.append([-self.literal_at(and_gate[0], depth), self.literal_at(and_gate[2], depth)])
-            self.cnf.append([self.literal_at(and_gate[0], depth), -self.literal_at(and_gate[1], depth), -self.literal_at(and_gate[2], depth)])   
+            clauses.append([-self.literal_at(and_gate[0], depth), self.literal_at(and_gate[1], depth)])  
+            clauses.append([-self.literal_at(and_gate[0], depth), self.literal_at(and_gate[2], depth)])
+            clauses.append([self.literal_at(and_gate[0], depth), -self.literal_at(and_gate[1], depth), -self.literal_at(and_gate[2], depth)])
+        return clauses
+    
+    def add_transition_system(self, depth: int):
+        for i in range(depth+1):
+            for clause in self.clauses_transision_system_at(i):
+                self.cnf.append(clause)
 
-    def add_output(self, depth: int):
+    def add_property(self, depth: int):
         # o^0 | o^1 | ... | o^depth
-        self.cnf.append([self.literal_at(self.circuit.output, i) for i in range(depth+1)])
+        self.property = [[self.literal_at(self.circuit.output, i) for i in range(depth+1)]]
+        for clause in self.property:
+            self.cnf.append(clause)
 
-    def run_solver(self) -> bool:
+    def is_sat(self) -> bool:
         self.cnf.to_file("input.txt")
-        returncode = run("MiniSat-p_v1.14/minisat input.txt > /dev/null", shell=True).returncode
+        returncode = run("MiniSat-p_v1.14/minisat input.txt -c > proof.txt", shell=True).returncode
         assert returncode in [10, 20]
-        run("rm input.txt", shell=True)
         return returncode == 10 # 10 if sat, 20 if unsat
-
-    def check(self, depth: int) -> bool:
+    
+    def holds_at(self, depth: int) -> bool:
         self.cnf = CNF()
         self.cnf.append([1]) # Aiger x_0 is always true (DIMACS literal 1)
 
         self.add_initial_state()        
-        for i in range(depth+1):
-            self.add_transition_system(i)
-        self.add_output(depth)
+        self.add_transition_system(depth)
+        self.add_property(depth)
 
-        return not self.run_solver()
+        return not self.is_sat()
+
+    def check(self, depth: int) -> bool:
+        for i in range(depth+1):
+            if not self.holds_at(i):
+                return False
+        return True
 
     def literal_at(self, literal: int, depth: int) -> int:
-        if literal in [-1, 1]: # constant case
+        assert literal != 0
+        if literal in [1, -1]: # in order to have only one constant clause in the CNF
             return literal
-        if literal < -1:
-            return literal - depth*(self.circuit.maxvar)
-        elif literal > 1:
-            return literal + depth*(self.circuit.maxvar)
+        
+        sign = 1 if literal > 0 else -1
+        atom = abs(literal)
+        atom_at_0 = (atom - 1)%self.circuit.maxvar + 1 # shifting because maxvar = output;
+        atom_at_depth = atom_at_0 + depth*self.circuit.maxvar
+        return sign*atom_at_depth
+        
+    def __del__(self):
+        pass
+        #run("rm input.txt proof.txt", shell=True)
         
 
 if __name__ == '__main__':
@@ -63,8 +84,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    circuit = AigerCircuit(args.filename)
-    if BoundedModelChecker(circuit).check(args.k):
+    if BoundedModelChecker(args.filename).check(args.k):
         print("OK")
     else:
         print("FAIL")
